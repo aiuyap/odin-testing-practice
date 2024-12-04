60462692a9d19a2ac2f7615c49bb267549154d44
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-04 11:19:14 +0800

    Created test and functon caesarCipher - Passed all tests

--- main.js
+++ main.js
@@ -1,3 +1,67 @@
 export function capitalize(str) {
   return str.toUpperCase();
 }
+
+export function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+export const calculator = () => {
+  const add = (x, y) => {
+    return x + y;
+  };
+
+  const sub = (x, y) => {
+    return x - y;
+  };
+
+  const mult = (x, y) => {
+    return x * y;
+  };
+
+  const div = (x, y) => {
+    return x / y;
+  };
+
+  return { add, sub, mult, div };
+};
+
+export const caesarCipher = (str, key) => {
+  const ABC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const encryptedABC = encryption(key, ABC);
+
+  const arrOfStr = str.split("");
+  const cipher = [];
+
+  for (let i = 0; i < arrOfStr.length; i++) {
+    let isUpperCase = false;
+    if (arrOfStr[i] >= "A" && arrOfStr[i] <= "Z") {
+      isUpperCase = true;
+    }
+    const currentChar = arrOfStr[i].toLowerCase();
+    const index = ABC.indexOf(currentChar);
+
+    if (index === -1) {
+      cipher.push(arrOfStr[i]);
+      continue;
+    }
+
+    if (isUpperCase) {
+      cipher.push(encryptedABC[index].toUpperCase());
+    } else {
+      cipher.push(encryptedABC[index]);
+    }
+  }
+
+  return cipher.join("");
+};
+
+function encryption(key, origABC) {
+  const ABC = origABC.slice();
+  for (let i = 0; i < key; i++) {
+    const x = ABC.shift();
+    ABC.push(x);
+  }
+
+  return ABC;
+}
--- main.test.js
+++ main.test.js
@@ -1,5 +1,43 @@
-import { capitalize } from "./main";
+import { capitalize, reverseString, calculator, caesarCipher } from "./main";
 
 test("Capitalize aiu -> AIU", () => {
   expect(capitalize("aiu")).toBe("AIU");
+});
+
+test("Reverse string `asdfgh`", () => {
+  expect(reverseString("asdfgH")).toBe("Hgfdsa");
+});
+
+const calc = calculator();
+
+test("Calc addition", () => {
+  expect(calc.add(4, 12)).toBe(16);
+});
+
+test("Calc subtraction", () => {
+  expect(calc.sub(12, 4)).toBe(8);
+});
+
+test("Calc division", () => {
+  expect(calc.div(12, 4)).toBe(3);
+});
+
+test("Calc multiplication", () => {
+  expect(calc.mult(4, 4)).toBe(16);
+});
+
+test("Caeser Cipher test", () => {
+  expect(caesarCipher("def", 3)).toBe("ghi");
+});
+
+test("Caeser Cipher wrapping test", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+});
+
+test("Caeser Cipher case preservation test", () => {
+  expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+});
+
+test("Caeser Cipher case preservation test", () => {
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
 });
